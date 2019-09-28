1e4f97ef49b8495599084c4daa04d9f13b2f8815
vishnu292 <47337198+vishnu292@users.noreply.github.com>
2019-09-27 21:45:04 -0400

    Everything until GUI

---
+++ Assets/Scripts/AppleTree.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = .02f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        // changing direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);// move left
+        }
+        
+
+        
+    }
+
+    void FixedUpdate() {
+        //changing direction randomly is now time based bc of fixed updates
+        if (Random.value < chanceToChangeDirections) {
+            speed *= -1; // chanceToChangeDirections direction
+
+        }
+    }
+}
